83190a9c474f0c8206495a53f0f6f1f06bc8b2b7
ChrisGGd <40560278+ChrisGGd@users.noreply.github.com>
2018-07-08 18:17:57 +0800

    Add files via upload

---
+++ build_your_starry_sky/sketch_jul07a.ino
@@ -0,0 +1,22 @@
+int valt;                        //输入参数进行调试
+#define anjian 2
+void setup() {
+  Serial.begin(9600);        // 打开串口设置波特率
+  pinMode(anjian,INPUT);
+}
+
+void   loop() {
+
+
+  if(digitalRead(anjian)  ==  HIGH)
+  {
+    valt  =  1;
+  }
+
+  Serial.print(valt);              // 送一个数到Processing
+  delay(300);
+valt=0;
+}
+
+
+
